348f3517d7f58558232ead6a6d9242fd1a991fe6
Jushuworld <jushuworld@gmail.com>
2019-08-30 16:28:25 -0700

    bug fixed

--- graphql/db.js
+++ graphql/db.js
@@ -27,6 +27,6 @@ export const people = [
 ];
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id)
+    const filteredPeople = people.filter(person => person.id === String.id)
     return filteredPeople[0]
 }
